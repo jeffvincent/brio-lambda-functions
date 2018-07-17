40a0f4f59da92519c651e991693188809b185f43
Jeff Vincent <jeff@wistia.com>
2018-07-17 13:40:53 -0400

    Update Typeform Submission script to support the event handler function.

--- typeformSubmission/typeformSubmission.js
+++ typeformSubmission/typeformSubmission.js
@@ -21,34 +21,53 @@ const responses = {
 
 // Lambda function
 exports.handler = (event, context, callback) => {
-  console.log('running event');
+  console.log('running event')
 
-  // End the lambda function when the send function completes.
-  forwardWithAuthentication(event, function(status) {
-    console.log("status: ", status);
-    if (status.error) {
-      return callback(null, responses.error(status));
-    }
-    if (event.body) {
-      sendInternalNotification(event, status);
-    }
-    return callback(null, responses.success(status));
-  });
+  if (!event.Records && !event.body) {
+    console.log('no data included with message, just returning.')
+    return callback(null, responses.success({ message: "no data included with message." }))
+  }
+
+  let snsMessage = event.Records[0].Sns.Message
+  console.log('sns message: ', snsMessage)
+
+  let parsedMessage = JSON.parse(snsMessage)
+
+  // send the event body along to Kinvey
+  forwardWithAuthentication(parsedMessage, function(status) {
+    console.log('forwarded and heres the status: ', status)
+
+    // notify Slack we've received a new Typeform
+    sendInternalNotification(parsedMessage, status, function(error) {
+      if (error) {
+        console.log('Slack notification error: ', error)
+        callback(null, responses.error({ message: error }))
+      }
+
+      console.log("Slack posted.")
+      callback(null, responses.success({ message: "Data passed to Kinvey" }))
+    })
+  })
 };
 
-function sendInternalNotification(event, status) {
-
-  let eventBody = JSON.parse(event.body);
-  console.log("eventBody form response answers ", eventBody.form_response.answers);
-
-  let submissionEmail = eventBody.form_response.answers.filter( answer => answer.type === 'email' )[0].email;
+function sendInternalNotification(parsedMessage, status, notificationCallback) {
+  console.log('posting to Slack: ', parsedMessage);
+  console.log('event_id: ', parsedMessage.event_id);
+  console.log('["event_id"]: ', parsedMessage["event_id"]);
+  let eventId = parsedMessage.event_id;
+  let submissionEmail = null;
+  if (parsedMessage.form_response && parsedMessage.form_response.answers) {
+    submissionEmail = parsedMessage.form_response.answers.filter( answer => answer.type === 'email' )[0].email;
+  }
 
   let messageBody = ""
-  messageBody += "Typeform submission received:"
+  messageBody += "Typeform submission received by AWS:"
   messageBody += " ```"
-  messageBody += `event id: ${event.event_id}\n`
-  messageBody += `email: ${submissionEmail}\n`
-  messageBody += `Kinvey returned ${status}.`
+  messageBody += `event id: ${eventId}\n`
+  if (submissionEmail) {
+    messageBody += `email: ${submissionEmail}\n`
+  }
+  messageBody += `event passed to Kinvey and returned status: ${status.message}`
   messageBody += "```"
 
   console.log(`notification messageBody: ${messageBody}`)
@@ -67,26 +86,24 @@ function sendInternalNotification(event, status) {
 
   rp(options)
     .then(parsedBody => {
-      console.log('slack post response: ', parsedBody)
-      return true
+      console.log('slack post response: ', parsedBody);
+      return notificationCallback(null);
     })
     .catch(err => {
-      console.log('err: ', err)
-      return true
+      console.log('slack post err: ', err);
+      return notificationCallback(err);
     })
 };
 
 
-function forwardWithAuthentication(body, completedCallback) {
-
-  console.log('forwarding!');
+function forwardWithAuthentication(parsedMessage, forwardingCallback) {
+  console.log('forwarding to Kinvey: ', parsedMessage);
 
   var options = {
     port: 443,
     uri: process.env.typeformSubmissionUrl,
     method: 'POST',
-    body: body,
-    json: true,
+    body: parsedMessage,
     headers: {
       'Content-Type': 'application/json',
       Authorization:
@@ -97,10 +114,12 @@ function forwardWithAuthentication(body, completedCallback) {
   rp(options)
     .then(parsedBody => {
       console.log('kinvey post response: ', parsedBody);
-      completedCallback({ message: parsedBody });
+      let res = { message: parsedBody };
+      return forwardingCallback(res);
     })
     .catch(err => {
-      console.log('err: ', err);
-      completedCallback({ error: true, message: err });
+      console.log('kinvey post err: ', err);
+      let res = { error: true, message: err };
+      return forwardingCallback(res);
     });
 }
