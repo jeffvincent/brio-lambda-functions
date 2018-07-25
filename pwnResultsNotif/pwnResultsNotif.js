ba194a75b08fdb4874db42af142468da50e09aa6
Jeff Vincent <jeff@wistia.com>
2018-07-25 14:57:19 -0400

    Prettier code.

--- pwnResultsNotif/pwnResultsNotif.js
+++ pwnResultsNotif/pwnResultsNotif.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
 // this function receives an order approved notification from PWN Health,
 // then notifies Kinvey of the approval status change.
 
 // libraries
-const rp = require('request-promise')
+const rp = require('request-promise');
 
 // create a responses object for use with the callback
 const responses = {
@@ -20,7 +20,7 @@ const responses = {
       body: error.message
     };
   }
-}
+};
 
 // Kinvey call options
 const kinveyOptions = {
@@ -31,9 +31,12 @@ const kinveyOptions = {
   headers: {
     'Content-Type': 'application/json',
     Authorization:
-      'Basic ' + new Buffer(process.env.kinvey_username + ':' + process.env.kinvey_password).toString('base64')
+      'Basic ' +
+      new Buffer(
+        process.env.kinvey_username + ':' + process.env.kinvey_password
+      ).toString('base64')
   }
-}
+};
 
 // slack call options
 const slackOptions = {
@@ -44,36 +47,51 @@ const slackOptions = {
   headers: {
     'Content-type': 'application/json'
   }
-}
+};
 
 // Lambda function
 exports.handler = (event, context, callback) => {
-  console.log('running event')
+  console.log('running event');
 
   // these are required query params by the API gateway
-  let token = event['queryStringParameters']['token']
-  let id = event['queryStringParameters']['id']
-  let result = event['queryStringParameters']['result'].toLowerCase()
-  let result_complete = event['queryStringParameters']['result_complete'].toLowerCase()
+  let token = event['queryStringParameters']['token'];
+  let id = event['queryStringParameters']['id'];
+  let result = event['queryStringParameters']['result'].toLowerCase();
+  let result_complete = event['queryStringParameters']['result_complete'].toLowerCase();
 
   //
   // ensure query param values are legit
   //
   if (token != process.env.authentication_token) {
-    console.log("attempt to call with incorrect token.")
-    return callback(null, responses.error({ code: 401, message: `incorrect token ${token}` }))
+    console.log('attempt to call with incorrect token.');
+    return callback(
+      null,
+      responses.error({ code: 401, message: `incorrect token ${token}` })
+    );
   }
 
-  let acceptableResults = 'released normal'.split(' ')
-  if ( acceptableResults.indexOf(result) < 0 ) {
-    console.log(`function called with incorrect result: ${result}`)
-    return callback(null, responses.error({ code: 400, message: `result type not recognized: ${result}` }))
+  let acceptableResults = 'released normal'.split(' ');
+  if (acceptableResults.indexOf(result) < 0) {
+    console.log(`function called with incorrect result: ${result}`);
+    return callback(
+      null,
+      responses.error({
+        code: 400,
+        message: `result type not recognized: ${result}`
+      })
+    );
   }
 
-  let acceptableResultCompleteTypes = 'incomplete complete'.split(' ')
-  if ( acceptableResultCompleteTypes.indexOf(result_complete) < 0 ) {
-    console.log(`function called with incorrect result_complete type: ${result_complete}`)
-    return callback(null, responses.error({ code: 400, message: `result_complete type not recognized: ${result_complete}` }))
+  let acceptableResultCompleteTypes = 'incomplete complete'.split(' ');
+  if (acceptableResultCompleteTypes.indexOf(result_complete) < 0) {
+    console.log(`incorrect result_complete type: ${result_complete}`);
+    return callback(
+      null,
+      responses.error({
+        code: 400,
+        message: `result_complete type not recognized: ${result_complete}`
+      })
+    );
   }
 
   //
@@ -83,89 +101,94 @@ exports.handler = (event, context, callback) => {
     orderId: id,
     result,
     result_complete
-  }
+  };
 
   // if the results are complete, request the results and append them onto the notification
   // TODO: this can definitely be cleaned up
   if (result_complete === 'complete') {
     requestResultsData(notification, function(data, err) {
       if (err) {
-        sendInternalNotification(notification, responses.error(err))
-        return callback(null, responses.error(err))
+        sendInternalNotification(notification, responses.error(err));
+        return callback(null, responses.error(err));
       }
 
       notifyKinvey(notification, function(status) {
-        sendInternalNotification(notification, status)
-        return callback(null, status)
-      })
-    })
+        sendInternalNotification(notification, status);
+        return callback(null, status);
+      });
+    });
   } else {
     // notify Kinvey of PWN order status change
     notifyKinvey(notification, function(status) {
-      return callback(null, status)
-    })
+      return callback(null, status);
+    });
   }
-}
+};
 
 function requestResultsData(notification, requestCallback) {
   // pwn health request options
   var options = {
     port: 443,
-    uri: `https://api16-staging.pwnhealth.com/customers/${notification.orderId}?include=reconciled_results`,
+    uri: `https://api16-staging.pwnhealth.com/customers/${
+      notification.orderId
+    }?include=reconciled_results`,
     method: 'GET',
     headers: {
       Authorization:
-        'Basic ' + new Buffer(process.env.pwn_key + ':' + process.env.pwn_token).toString('base64')
+        'Basic ' +
+        new Buffer(
+          process.env.pwn_key + ':' + process.env.pwn_token)
+        .toString('base64')
     }
-  }
+  };
 
   rp(options)
     .then(parsedBody => {
-      console.log('body: ', parsedBody)
-      notification.results_data = parsedBody
-      requestCallback(notification, null)
+      console.log('body: ', parsedBody);
+      notification.results_data = parsedBody;
+      requestCallback(notification, null);
     })
     .catch(err => {
-      console.log('err: ', err)
-      requestCallback(null, responses.error(err))
-    })
+      console.log('err: ', err);
+      requestCallback(null, responses.error(err));
+    });
 }
 
 function sendInternalNotification(notification, status) {
-  let messageBody = ""
-  messageBody += "PWN event received:"
-  messageBody += " ```"
-  messageBody += `type: results received\n`
-  messageBody += `order ID: ${notification.orderId}\n`
-  messageBody += `Kinvey returned ${status.statusCode}: \"${status.body.replace(/\./g, '')}\", and sent proper callback.`
-  messageBody += "``` "
+  let messageBody = '';
+  messageBody += 'PWN event received:';
+  messageBody += ' ```';
+  messageBody += `type: results received\n`;
+  messageBody += `order ID: ${notification.orderId}\n`;
+  messageBody += `Kinvey returned ${status.statusCode}:`;
+  messageBody += `\"${status.body.replace(/\./g, '')}\", proper callback sent.`;
+  messageBody += '``` ';
 
-  console.log(`notification messageBody: ${messageBody}`)
+  console.log(`notification messageBody: ${messageBody}`);
 
-  slackOptions.body = { "text": messageBody };
+  slackOptions.body = { text: messageBody };
 
   rp(slackOptions)
     .then(parsedBody => {
-      console.log('body: ', parsedBody)
-      return true
+      console.log('body: ', parsedBody);
+      return true;
     })
     .catch(err => {
-      console.log('err: ', err)
-      return true
-    })
+      console.log('err: ', err);
+      return true;
+    });
 }
 
 function notifyKinvey(notification, completedCallback) {
-
   kinveyOptions.body = notification;
 
   rp(kinveyOptions)
     .then(parsedBody => {
-      console.log('body: ', parsedBody)
-      completedCallback(responses.success(parsedBody))
+      console.log('body: ', parsedBody);
+      completedCallback(responses.success(parsedBody));
     })
     .catch(err => {
-      console.log('err: ', err)
-      completedCallback(responses.error(err))
-    })
+      console.log('err: ', err);
+      completedCallback(responses.error(err));
+    });
 }
